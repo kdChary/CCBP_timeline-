077fb5f841cc30317cfb8832d58950dc1d7ff325
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-04-26 03:39:18 +0000

    coursseTimeLinecard complete

--- src/components/CourseTimelineCard/index.js
+++ src/components/CourseTimelineCard/index.js
@@ -1,1 +1,37 @@
-// Write your code here
+// importing Icons
+import {BsClockFill} from 'react-icons/bs'
+
+// importing styling from css file
+import './index.css'
+
+const CourseTimelineCard = props => {
+  const {course} = props
+  const {courseTitle, description, duration, tagsList} = course
+
+  const courseTags = () => (
+    <ul className="course-tags-list">
+      {tagsList.map(tag => (
+        <li key={tag.id} className="course-tag">
+          <p className="tag">{tag.name}</p>
+        </li>
+      ))}
+    </ul>
+  )
+
+  return (
+    <div className="course-item">
+      <div className="course-title-duration">
+        <h2 className="course-title">{courseTitle}</h2>
+        <p className="course-duration">
+          <BsClockFill />
+          {duration}
+        </p>
+      </div>
+
+      <p className="course-description">{description}</p>
+      {courseTags()}
+    </div>
+  )
+}
+
+export default CourseTimelineCard
--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -1,1 +1,31 @@
-// Write your code here
+// Importing dependencies.
+import {Chrono} from 'react-chrono'
+
+import './index.css'
+import CourseTimelineCard from '../CourseTimelineCard'
+
+const TimelineView = props => {
+  const {timelineItemsList} = props
+  const courseList = timelineItemsList.filter(
+    item => item.categoryId === 'COURSE',
+  )
+  const projectsList = timelineItemsList.filter(
+    item => item.categoryId === 'PROJECT',
+  )
+  const titles = timelineItemsList.map(item => ({title: item.title}))
+
+  console.log(courseList, projectsList)
+
+  return (
+    <div className="chrono-container">
+      <Chrono mode="VERTICAL_ALTERNATING" items={titles}>
+        {courseList.map(courses => (
+          <CourseTimelineCard key={courses.id} course={courses} />
+        ))}
+        <p>Hello2</p>
+      </Chrono>
+    </div>
+  )
+}
+
+export default TimelineView
